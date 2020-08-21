5ee6178de436ac7d43ec8a832edf1e62aa2e011e
Mikołaj Stempin <m.stempin99@gmail.com>
2020-08-21 11:57:54 +0200

    Added test to method GetAllBooks

--- demo/src/test/java/com/example/demo/Service/BookControllerTest.java
+++ demo/src/test/java/com/example/demo/Service/BookControllerTest.java
@@ -1,23 +1,35 @@
 package com.example.demo.Service;
 
 import com.example.Controller.BookController;
+import com.example.Model.Book;
 import com.example.Repository.BookRepository;
+import com.example.Service.BookService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class BookControllerTest {
 
     @Mock
-    private BookRepository repository;
+    private BookService bookService;
 
     @InjectMocks
     private BookController controller;
+
+    @Test
+    public void whenGetAllBooks_ThenBookShowAllTriggered() {
+        assertThat(controller.getAllBooks()).isEqualTo(bookService.bookShowAll());
+     }
 }
