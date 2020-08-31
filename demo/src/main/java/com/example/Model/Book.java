da5a8c29237169dc3e1bfa67ccbef1d7e27de6c3
Mikołaj Stempin <m.stempin99@gmail.com>
2020-08-31 10:51:37 +0200

    Builder pattern

--- demo/src/main/java/com/example/Model/Book.java
+++ demo/src/main/java/com/example/Model/Book.java
@@ -2,22 +2,15 @@ package com.example.Model;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import java.util.Arrays;
-import java.util.List;
 
 @Entity
 public class Book {
 
-
     @Id
     private String title;
-
     private String author;
-
     private Integer number_of_pages;
-
     private Integer rating;
-
     private String ISBN;
 
     public String getTitle() {
@@ -60,7 +53,51 @@ public class Book {
         this.rating = rating;
     }
 
-    public List<String> bookToList() {
-        return Arrays.asList(getTitle(), getAuthor(), getNumber_of_pages().toString(), getRating().toString(), getISBN());
+    public static class Builder {
+
+        private String title;
+        private String author;
+        private Integer number_of_pages;
+        private Integer rating;
+        private String ISBN;
+
+        public Builder(String title) {
+            this.title = title;
+        }
+
+        public Builder buildAuthor(String author){
+            this.author = author;
+
+            return this;
+        }
+
+        public Builder buildNumber_of_pages(Integer number_of_pages){
+            this.number_of_pages = number_of_pages;
+
+            return this;
+        }
+
+        public Builder buildRating(Integer rating){
+            this.rating = rating;
+
+            return this;
+        }
+
+        public Builder buildISBN(String ISBN){
+            this.ISBN = ISBN;
+
+            return this;
+        }
+
+        public Book build(){
+            Book book = new Book();
+            book.title = this.title;
+            book.author = this.author;
+            book.number_of_pages = this.number_of_pages;
+            book.rating = this.rating;
+            book.ISBN = this.ISBN;
+
+            return book;
+        }
     }
 }
