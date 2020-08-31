48ffcddef36852f54de4576ad5752d8aa9c62221
Mikołaj Stempin <m.stempin99@gmail.com>
2020-08-31 10:51:22 +0200

    Lombok annotation
    added Lombok annotation

--- demo/src/main/java/com/example/Controller/BookController.java
+++ demo/src/main/java/com/example/Controller/BookController.java
@@ -6,14 +6,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.transaction.Transactional;
+import lombok.extern.log4j.Log4j2;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+@Log4j2
 @Controller
 @RequestMapping(path="/books")
 public class BookController {
-    private static final Logger LOG = LoggerFactory.getLogger(BookController.class);
+
+    private static final org.apache.logging.log4j.Logger log = org.apache.logging.log4j.LogManager.getLogger(BookController.class);
 
     private final BookService bookService;
 
@@ -23,7 +23,7 @@ public class BookController {
     public @ResponseBody void addNewBook (@RequestBody Book book) {
         bookService.bookSave(book);
 
-        LOG.info("Book with ISBN -> {} <- has been ADDED", book.getISBN());
+        log.info("Book with ISBN -> {} <- has been ADDED", book.getISBN());
     }
 
     @GetMapping(path="/all")
@@ -34,9 +34,12 @@ public class BookController {
     @DeleteMapping(path="/delete")
     @Transactional
     public @ResponseBody void deleteBook (@RequestBody Book book) {
+        try{
         bookService.bookDelete(book);
-
-        LOG.info("Book with ISBN -> {} <- has been DELETED", book.getISBN());
+        log.info("Book with ISBN -> {} <- has been DELETED", book.getISBN());}
+        catch (Exception e){
+            log.error("Exeption " + e + " has occurred");
+        }
     }
 
 }
