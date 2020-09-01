c1f4964bb132d0320d740bf47e7de315f3867606
Mikołaj Stempin <m.stempin99@gmail.com>
2020-09-01 11:57:03 +0200

    ISBN validator
    method validating ISBN number

--- demo/src/main/java/com/example/Service/BookService.java
+++ demo/src/main/java/com/example/Service/BookService.java
@@ -22,4 +22,31 @@ public class BookService {
     public Iterable<Book> bookShowAll() {
         return bookRepository.findAll();
     }
+
+    public boolean ISBN_validator_check(Book book) {
+        String ISBN = book.getISBN();
+        int n = ISBN.length();
+
+        if (n != 10)
+            return false;
+
+        int sum = 0;
+        for (int i = 0; i < 9; i++)
+        {
+            int digit = ISBN.charAt(i) - '0';
+            if (0 > digit || 9 < digit)
+                return false;
+            sum += (digit * (10 - i));
+        }
+
+        char last = ISBN.charAt(9);
+        if (last != 'X' && (last < '0' ||
+                            last > '9'))
+            return false;
+
+        sum += ((last == 'X') ? 10 : (last - '0'));
+
+        return (sum % 11 == 0);
+    }
+
 }
